bd69c3afa19da0ecdebba51737d0cd1f75b7a105
michael-duke <michael.21duke@gmail.com>
2022-11-14 16:20:30 +0300

    Rearrange the foreign keys, table names should have an 's' at the end Add index to enhance performance

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
-
+DROP TABLE IF EXISTS games;
+DROP TABLE IF EXISTS music_albums;
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS labels;
+DROP TABLE IF EXISTS authors;
 -- Authors
 CREATE TABLE authors (
   id BIGSERIAL NOT NULL PRIMARY KEY,
@@ -17,7 +22,7 @@ CREATE TABLE labels (
 -- Genres
 CREATE TABLE genres (
   id BIGSERIAL NOT NULL PRIMARY KEY,
-  name VARCHAR(150) NOT NULL,
+  name VARCHAR(150) NOT NULL
 );
 
 -- Books
@@ -25,9 +30,9 @@ CREATE TABLE books (
   id BIGSERIAL NOT NULL PRIMARY KEY,
   publisher VARCHAR(150) NOT NULL,
   cover_state VARCHAR(150) NOT NULL,
-  genre_id INT NULL REFERENCES genre(id) ON DELETE CASCADE,
-  author_id INT NULL REFERENCES author(id) ON DELETE CASCADE,
-  label_id INT NULL REFERENCES label(id) ON DELETE CASCADE,
+  genre_id INT NULL REFERENCES genres(id) ON DELETE CASCADE,
+  author_id INT NULL REFERENCES authors(id) ON DELETE CASCADE,
+  label_id INT NULL REFERENCES labels(id) ON DELETE CASCADE,
   publish_date DATE NOT NULL,
   archived BOOLEAN NOT NULL
 );
@@ -36,22 +41,26 @@ CREATE TABLE books (
 CREATE TABLE music_albums (
   id BIGSERIAL NOT NULL PRIMARY KEY,
   on_spotify BOOLEAN NOT NULL,
-  genre_id INT NULL REFERENCES genre(id) ON DELETE CASCADE,
-  author_id INT NULL REFERENCES author(id) ON DELETE CASCADE,
-  label_id INT NULL REFERENCES label(id) ON DELETE CASCADE,
+  genre_id INT NULL REFERENCES genres(id) ON DELETE CASCADE,
+  author_id INT NULL REFERENCES authors(id) ON DELETE CASCADE,
+  label_id INT NULL REFERENCES labels(id) ON DELETE CASCADE,
   publish_date DATE NOT NULL,
   archived BOOLEAN NOT NULL
 );
 
 -- Games
-
 CREATE TABLE games (
   id BIGSERIAL NOT NULL PRIMARY KEY,
   multiplayer VARCHAR(150) NOT NULL,
   last_played_at DATE NOT NULL,
-  genre_id INT NULL REFERENCES genre(id) ON DELETE CASCADE,
-  author_id INT NULL REFERENCES author(id) ON DELETE CASCADE,
-  label_id INT NULL REFERENCES label(id) ON DELETE CASCADE,
+  genre_id INT NULL REFERENCES genres(id) ON DELETE CASCADE,
+  author_id INT NULL REFERENCES authors(id) ON DELETE CASCADE,
+  label_id INT NULL REFERENCES labels(id) ON DELETE CASCADE,
   publish_date DATE NOT NULL,
   archived BOOLEAN NOT NULL
-)
+);
+
+-- Create index to enhance performance
+CREATE INDEX music_album_genre_id ON music_albums(genre_id);
+CREATE INDEX game_author_id ON games(author_id);
+CREATE INDEX book_label_id ON books(label_id);
