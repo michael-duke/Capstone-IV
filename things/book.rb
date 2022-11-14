49360d65d304e03ca149da7cc503742673eea0cd
michael-duke <michael.21duke@gmail.com>
2022-11-14 16:47:44 +0300

    Create Book class and implement the override the can_be_archived? method

---
+++ things/book.rb
@@ -0,0 +1,17 @@
+require_relative '../item.rb'
+
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(publisher, cover_state, *args)
+    super(*args)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  private
+
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
+end
