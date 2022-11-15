3343e7817ae6dd74b9d516727dcc31e71074301d
Inieke Sendy <ini.e.sendy@gmail.com>
2022-11-15 13:41:46 +0100

    [game_class]: Fix: arg

--- things/game.rb
+++ things/game.rb
@@ -2,8 +2,8 @@ require 'date'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(multiplayer, last_played_at)
-    super
+  def initialize(multiplayer, last_played_at, *args)
+    super(*args)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
