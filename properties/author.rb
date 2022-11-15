647a2c5218cc6c4b4dd642d4039c30d35128e618
Inieke Sendy <ini.e.sendy@gmail.com>
2022-11-15 06:22:28 +0100

    [feature_author]: Add: Instances

--- properties/author.rb
+++ properties/author.rb
@@ -2,4 +2,15 @@ require 'securerandom'
 
 class Author
 
+  attr_accessor :first_name, :last_name
+  attr_reader :id, :items
+
+  def initialize(first_name, last_name)
+    @id = SecureRandom.hex(5)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+
 end
