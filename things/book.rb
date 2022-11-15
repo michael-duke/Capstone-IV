d4af89bf689f25176151f635d9ef31127a4b6f42
michael-duke <michael.21duke@gmail.com>
2022-11-15 17:10:16 +0300

    Adjust required changes from coding partner

--- things/book.rb
+++ things/book.rb
@@ -1,7 +1,7 @@
 require_relative '../item'
 
 class Book < Item
-  attr_accessor :publisher, :cover_state, :label, :genre
+  attr_accessor :publisher, :cover_state, :label, :genre, :author
 
   def initialize(publisher, cover_state, *args)
     super(*args)
