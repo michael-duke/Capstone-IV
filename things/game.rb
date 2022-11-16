ff0d4227fded2cd77fbf6d9114195ac5500ab415
Inieke Sendy <ini.e.sendy@gmail.com>
2022-11-16 10:28:50 +0100

    [preserve_data]: Update: constructor listing *arg

--- things/game.rb
+++ things/game.rb
@@ -1,16 +1,19 @@
 require 'date'
+require_relative '../item.rb'
+
+
 class Game < Item
   attr_accessor :multiplayer, :last_played_at, :label, :author, :genre
 
-  def initialize(multiplayer, last_played_at, *args)
+  def initialize(*args, last_played_at, multiplayer: false)
     super(*args)
-    @multiplayer = multiplayer
     @last_played_at = last_played_at
+    @multiplayer = multiplayer
   end
 
   private
 
   def can_be_archived?
-    super and Date.today.year - Date.parse(@last_played_at).year > 2
+    super and (Date.today.year - Date.parse(@last_played_at).year > 2)
   end
 end
