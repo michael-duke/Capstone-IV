d84e7b00195c70a22c782c37ee098a083f9ba290
Sendy Inieke <95294594+uisendy@users.noreply.github.com>
2022-11-16 11:24:15 +0100

    [preserve_data]: fix: linters

--- app.rb
+++ app.rb
@@ -212,6 +212,9 @@ class App
       puts "Authors list, count(#{@authors.count})👤 :\n\n"
       @authors.each_with_index do |author, index|
         puts "#{index + 1}) Author: #{author.first_name}, #{author.last_name}"
+      end
+    end
+  end
 
   def list_all_genres
     if @genres.empty?
